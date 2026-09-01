729db95fd8b9642158a12b22f7187ae6704513f2
daniilkrivenya-ship-it <daniilkrivenya@gmail.com>
2026-09-01 21:58:15 +0300

    Add VK pixel only

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,3 +263,14 @@
     banner.classList.remove('is-visible');
   });
 })();
+
+// Top.Mail.Ru / VK counter
+var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3789701", type: "pageView", ecommerce: "sign_up_page", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "tmr-code");
